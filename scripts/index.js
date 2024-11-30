bd41dc9281dff8016765a15862a9ce74d71df233
dbandz <protaindevin@gmail.com>
2024-11-30 11:54:28 -0500

    sprint 5 project corrections

--- scripts/index.js
+++ scripts/index.js
@@ -34,12 +34,12 @@ const profileDescription = document.querySelector(".profile__description");
 const editModal = document.querySelector("#edit-modal");
 
 const editFormElement = editModal.querySelector(".modal__form");
-const closeModalButton = document.querySelector(".modal__close-btn");
+const profileCloseBtn = document.querySelector("#profile-close-btn");
 const editModalNameInput = document.querySelector("#name");
 const editModalDescriptionInput = document.querySelector("#description");
 
 const cardModal = document.querySelector("#add-card-modal");
-const cardCloseButton = cardModal.querySelector(".modal__close-btn");
+const cardCloseButton = cardModal.querySelector("#card-close-btn");
 const cardForm = cardModal.querySelector(".modal__form");
 const cardLinkInput = cardModal.querySelector("#add-card-link");
 const cardNameInput = cardModal.querySelector("#add-card-name");
@@ -50,9 +50,8 @@ const cardsList = document.querySelector(".cards__list");
 const cardPreviewModal = document.querySelector("#preview-modal");
 const cardImagePreview = cardPreviewModal.querySelector(".modal__image");
 const cardCaptionPreview = cardPreviewModal.querySelector(".modal__caption");
-const previewModalCloseBtn = cardPreviewModal.querySelector(
-  ".modal__close-btn-preview"
-);
+const previewModalCloseBtn =
+  cardPreviewModal.querySelector("#preview-close-btn");
 
 function getCardElement(data) {
   const cardElement = cardTemplate.content
@@ -96,7 +95,7 @@ profileEditButton.addEventListener("click", () => {
   openModal(editModal);
 });
 
-closeModalButton.addEventListener("click", () => {
+profileCloseBtn.addEventListener("click", () => {
   closeModal(editModal);
 });
 
@@ -128,6 +127,7 @@ function handleCardFormSubmit(evt) {
   const inputValues = { name: cardNameInput.value, link: cardLinkInput.value };
   const cardElement = getCardElement(inputValues);
   cardsList.prepend(cardElement);
+  evt.target.reset();
   closeModal(cardModal);
 }
 
