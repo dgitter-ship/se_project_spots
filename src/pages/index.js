96327b67bc8a24ae33bef2a71fcaf7f84c934a69
dbandz <protaindevin@gmail.com>
2025-04-26 17:01:04 -0400

    handleLike

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,18 +55,20 @@ api
       const cardElement = getCardElement(item);
       cardsList.prepend(cardElement);
     });
-    users.forEach(() => {
-      editModalNameInput.value = profileName.textContent;
-      editModalDescriptionInput.value = profileDescription.textContent;
-      resetValidation(
-        editFormElement,
-        [editModalNameInput, editModalDescriptionInput],
-        settings
-      );
-    });
+    // set the name element's text content
+    // set desc. element's text content
+    // set the avatar src
     //   // handle users info
     //   // - set src of avatar img
     //   // -set the textContent  of both text elements
+    // function handleAvatarFormSubmit(evt) {
+    //   evt.preventDefault();
+    //   const avatarInputValues = {
+    //     link: profileAvatarImg.value,
+    //   };
+    //   const cardElement = getCardElement(inputValues);
+    //   cardsList.prepend(cardElement);
+    // }
   })
   .catch(console.error);
 
@@ -90,6 +92,9 @@ const cardSubmitButton = cardModal.querySelector(".modal__submit-btn");
 const cardLinkInput = cardModal.querySelector("#add-card-link");
 const cardNameInput = cardModal.querySelector("#add-card-name");
 
+const deleteModal = document.querySelector("#delete-modal");
+const deleteForm = deleteModal.querySelector(".modal__form");
+
 // avatar form els
 const avatarModal = document.querySelector("#avatar-modal");
 const avatarCloseButton = avatarModal.querySelector("#avatar-close-btn");
@@ -109,6 +114,36 @@ const previewModalCloseBtn =
 
 const modalOverlays = document.querySelectorAll(".modal");
 
+let selectedCard, selectedCardId;
+
+function handleDeleteSubmit(evt) {
+  evt.preventDefault();
+  api
+    .deleteCard(selectedCardId)
+    .then(() => {
+      // TODO - remove card from Dom
+      // close modal
+      selectedCard.remove();
+      closeModal(deleteModal);
+    })
+    .catch(console.error);
+}
+
+function handleDeleteCard(cardElement, cardId) {
+  selectedCard = cardElement;
+  selectedCardId = cardId;
+  openModal(deleteModal);
+  console.log(cardId);
+}
+
+function handleLikeBtn(evt, id) {
+  // evt.target.classList.toggle("card__like-btn_liked");
+  // TODO -check whether card is like.
+  // const isLiked = evt.target;
+  // call handleLike function passing appropriate arguments
+  // handle res
+}
+
 function getCardElement(data) {
   const cardElement = cardTemplate.content
     .querySelector(".card")
@@ -119,17 +154,19 @@ function getCardElement(data) {
   const cardLikeButton = cardElement.querySelector(".card__like-btn");
   const cardDeleteButton = cardElement.querySelector(".card__delete-btn");
 
+  // if card is liked set liked class to card
+
   cardNameElement.textContent = data.name;
   cardImageElement.src = data.link;
   cardImageElement.alt = data.name;
 
-  cardLikeButton.addEventListener("click", () => {
-    cardLikeButton.classList.toggle("card__like-btn_liked");
-  });
+  cardLikeButton.addEventListener("click", (evt) =>
+    handleLikeBtn(evt, data._id)
+  );
 
-  cardDeleteButton.addEventListener("click", () => {
-    cardElement.remove();
-  });
+  cardDeleteButton.addEventListener("click", () =>
+    handleDeleteCard(cardElement, data._id)
+  );
 
   cardImageElement.addEventListener("click", () => {
     openModal(cardPreviewModal);
@@ -226,12 +263,29 @@ function handleEditFormSubmit(evt) {
 
 function handleCardFormSubmit(evt) {
   evt.preventDefault();
-  const inputValues = { name: cardNameInput.value, link: cardLinkInput.value };
-  const cardElement = getCardElement(inputValues);
-  cardsList.prepend(cardElement);
-  evt.target.reset();
-  disableButton(cardSubmitButton, settings);
-  closeModal(cardModal);
+  api
+    .postUserCard({
+      name: cardNameInput.value,
+      link: cardLinkInput.value,
+    })
+    .then((data) => {
+      const inputValues = {
+        name: cardNameInput.value,
+        link: cardLinkInput.value,
+      };
+      const cardElement = getCardElement(inputValues);
+      cardsList.prepend(cardElement);
+      evt.target.reset();
+      disableButton(cardSubmitButton, settings);
+      closeModal(cardModal);
+    })
+    .catch(console.error);
+  // const inputValues = { name: cardNameInput.value, link: cardLinkInput.value };
+  // const cardElement = getCardElement(inputValues);
+  // cardsList.prepend(cardElement);
+  // evt.target.reset();
+  // disableButton(cardSubmitButton, settings);
+  // closeModal(cardModal);
 }
 // todo - handleAvatarFormSubmit()
 
@@ -248,5 +302,6 @@ function handleAvatarFormSubmit(evt) {
 editFormElement.addEventListener("submit", handleEditFormSubmit);
 cardForm.addEventListener("submit", handleCardFormSubmit);
 avatarForm.addEventListener("submit", handleAvatarFormSubmit);
+deleteForm.addEventListener("submit", handleDeleteSubmit);
 
 envableValidation(settings);
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -12,7 +12,7 @@ class Api {
       if (res.ok) {
         return res.json();
       }
-      Promise.reject(`Error: $(res.status)`);
+      Promise.reject(`Error: ${res.status}`);
     });
   }
 
@@ -31,6 +31,26 @@ class Api {
       Promise.reject(`Error: $(res.status)`);
     });
   }
+
+  // TODO - Implement posting new cards
+
+  postUserCard({ name, link }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      // Send the data in the body as a JSON string.
+      body: JSON.stringify({
+        name,
+        link,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      Promise.reject(`Error: $(res.status)`);
+    });
+  }
+
   editUserInfo({ name, about }) {
     return fetch(`${this._baseUrl}/users/me`, {
       method: "PATCH",
@@ -56,6 +76,29 @@ class Api {
       body: JSON.stringify({
         avatar,
       }),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      Promise.reject(`Error: $(res.status)`);
+    });
+  }
+  deleteCard(id) {
+    return fetch(`${this._baseUrl}/cards/${id}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      Promise.reject(`Error: $(res.status)`);
+    });
+  }
+
+  handleLike(id, isLiked) {
+    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+      method: isLiked ? "DELETE" : "PUT",
+      headers: this._headers,
     }).then((res) => {
       if (res.ok) {
         return res.json();
